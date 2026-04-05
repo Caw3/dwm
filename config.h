04713a1deb09a518be9cb5a37a19cae7022c800b
Caw3 <carl@willman.nu>
2026-04-05 12:55:28 +0200

    update nord scheme

--- config.h
+++ config.h
@@ -9,15 +9,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#3b4252";
+static const char col_gray3[]       = "#434c5e";
+static const char col_gray4[]       = "#4c566a";
+static const char col_cyan[]        = "#88c0d0";
+static const char col_orange[]        = "#d08770";
+static const char col_white[]        = "#d8dee9";
+static const char col_purple[]        = "#b48ead";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_white, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_purple, col_gray4,  col_cyan  },
 };
 
 /* tagging */
